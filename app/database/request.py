aea267ef434db4971930b1071ce7472aeb131c2d
Felix <93269319+FelixCmmrsv@users.noreply.github.com>
2023-11-19 19:31:55 +0600

    I believe i can fly

--- app/database/request.py
+++ app/database/request.py
@@ -1,5 +1,5 @@
 from app.database.models import User, Queue, Group, Follow, async_session
-from sqlalchemy import select
+from sqlalchemy import select, desc
 import logging
 
 
@@ -42,6 +42,7 @@ async def add_queue(chat_id, queue_name, size):
             else:
                 return True
 
+
 async def add_user(new_id, user_name):
     async with async_session() as session:
         async with session.begin():
@@ -56,7 +57,8 @@ async def add_user(new_id, user_name):
                 return False
             else:
                 return True
-            
+
+
 async def add_chat(chat_id, chat_name):
     async with async_session() as session:
         async with session.begin():
@@ -82,7 +84,7 @@ async def get_user_nickname(user_id):
 
 async def get_positions(queue_id):
     async with async_session() as session:
-        result = await session.execute(select(Follow).filter(Follow.following_queue_id == queue_id))
+        result = await session.execute(select(Follow).filter(Follow.following_queue_id == int(queue_id)))
         positions = result.scalars().all()
         return positions
 
@@ -93,3 +95,44 @@ async def get_queue(queue_id):
         queue = result.scalar()
         return queue
 
+
+async def get_queues(chat_id):
+    async with async_session() as session:
+        result = await session.execute(select(Queue).filter(Queue.chat_id == chat_id))
+        queues = result.scalars().all()
+        return queues
+
+
+async def get_queue_max_size(queue_id):
+    async with async_session() as session:
+        result = await session.execute(select(Queue.size).filter(Queue.id == queue_id))
+        max_position = result.scalar()
+        return max_position
+
+
+async def join_queue(user_id, chat_id, queue_name):
+    async with async_session() as session:
+        async with session.begin():
+            result = await session.execute(
+                select(Queue.id).where(Queue.chat_id == chat_id, Queue.queue_name == queue_name)
+            )
+            queue_id = result.scalar()
+
+            follow_exists_result = await session.execute(
+                select(Follow).where(Follow.following_user_id == user_id,
+                                     Follow.following_queue_id == queue_id)
+            )
+            follow_exists = follow_exists_result.scalar()
+
+            if follow_exists is None:
+                position_result = await session.execute(
+                    select(Follow.position).where(Follow.following_queue_id == queue_id).order_by(desc(Follow.position))
+                )
+                position = position_result.scalars().first()
+                if position is None:
+                    position = 0
+                follow_row = Follow(following_user_id=user_id, following_queue_id=queue_id, position=position + 1)
+                session.add(follow_row)
+                return False
+            else:
+                return True
--- app/handlers.py
+++ app/handlers.py
@@ -1,7 +1,6 @@
 from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
-from tabulate import tabulate
 from instances_for_main import bot
 
 import logging
@@ -16,7 +15,7 @@ QUEUE_NAME = []
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     chat_type = message.chat.type
-    if chat_type == 'supergroup':
+    if chat_type == 'supergroup' or chat_type == 'group':
         chat_id = message.chat.id
         chat_name = message.chat.title
         if not await rq.add_chat(chat_id, chat_name):
@@ -24,10 +23,10 @@ async def cmd_start(message: Message):
         else:
             await message.answer(f"Chat was already in db: {chat_id}, {chat_name}.")
         if not await rq.add_user(message.from_user.id, message.from_user.first_name):
-            await message.answer(f'Welcome, {message.from_user.first_name}', reply_markup=await kb.main())
+            await message.answer(f'Welcome, {message.from_user.first_name}', reply_markup=kb.main)
         else:
             nickname = await rq.get_user_nickname(message.from_user.id)
-            await message.answer(f'Welcome back, {nickname}!', reply_markup=await kb.main())
+            await message.answer(f'Welcome back, {nickname}!', reply_markup=kb.main)
     else:
         if not await rq.add_user(message.from_user.id, message.from_user.first_name):
             await message.answer(f'Welcome, {message.from_user.first_name}', reply_markup=await kb.private_main())
@@ -35,30 +34,23 @@ async def cmd_start(message: Message):
             nickname = await rq.get_user_nickname(message.from_user.id)
             await message.answer(f'Welcome back, {nickname}!', reply_markup=await kb.private_main())
 
-# # Command to retrieve the stored queue name
-# @router.message(Command('get_queue_name'))
-# async def cmd_get_queue_name(message: Message):
-#     # Retrieve the user's queue name from the storage
-#     user_id = message.from_user.id
-#     user_data = await storage.get_data(chat=user_id)
-#     queue_name = user_data.get('queue_name', 'No queue name saved yet.')
-
-#     # Send the user's queue name
-#     await message.answer(f"Your queue name is: {queue_name}")
 
 @router.message(Command('showqueues'))
 async def cmd_show_all(message: Message):
     queues = await rq.get_group_queues(message.chat.id)
     await message.answer(str(queues))
 
-@router.message(Command('createqueue'))
+
+@router.callback_query(F.data == 'create_queue')
 async def create_queue(message: Message):
+    ALLOWED_USER.clear()
     if len(ALLOWED_USER) == 0:
         logging.info(f"Received message: {message.from_user.id}")
         ALLOWED_USER.append(message.from_user.id)
-        await message.answer("Enter queue name: [!name ______]")
+        await message.answer('Enter queue name: [!name ______]')
     else:
         await message.answer("Previous queue has not been named yet.")
+
 
 @router.message()
 async def naming_queue(message: Message):
@@ -75,32 +67,54 @@ async def naming_queue(message: Message):
             await rq.add_queue(chat_id=message.chat.id, queue_name=text, size=size)
             await message.answer(f"{QUEUE_NAME[0]} has been added!")
 
+
 @router.callback_query(F.data == 'show_queue')
 async def queue(callback: CallbackQuery):
-    await callback.message.answer('Choose a queue to see more information', reply_markup=await kb.show_queue())
+    chat_id = callback.message.chat.id
+    await callback.message.answer('Choose a queue to see more information', reply_markup=await kb.show_queue(chat_id))
 
 
 @router.callback_query(F.data.startswith('queue_'))
 async def queue_selected(callback: CallbackQuery):
-    queue_id = callback.data.split('_')[1]
+    queue_id_str = callback.data.split('_')[1]
+    queue_id = int(queue_id_str)
     positions = await rq.get_positions(queue_id)
     queue_name = await rq.get_queue(queue_id)
 
-    # Create a dictionary to map positions to nicknames
-    pos_to_nickname = {}
+    pos_to_nick = {}
+
     for position in positions:
         user_id = position.following_user_id
-        pos = position.position
+        user_position = position.position
         nickname = await rq.get_user_nickname(user_id)
-        pos_to_nickname[pos] = nickname
+        pos_to_nick[user_position] = nickname
 
-    # Generate the formatted output
-    max_position = max(pos_to_nickname.keys(), default=0)
-    output_lines = [f"{queue_name.queue_name} selected:"]
-    for i in range(1, max_position + 1):
-        nickname = pos_to_nickname.get(i, "*blank*")
-        output_lines.append(f"{str(i)}. {nickname}")
+    max_pos = await rq.get_queue_max_size(queue_id)
+    output_lines = [f'{queue_name} positions:']
+    for i in range(1, int(max_pos) + 1):
+        nick = pos_to_nick.get(i, ' ')
+        output_lines.append(f'{i}. {nick}')
 
     formatted_output = '\n'.join(output_lines)
-
     await callback.message.answer(formatted_output)
+
+
+@router.callback_query(F.data == 'join_queue')
+async def join(callback: CallbackQuery):
+    chat_id = callback.message.chat.id
+    await callback.message.answer('Choose a queue to join', reply_markup=await kb.join_queue(chat_id))
+
+
+@router.callback_query(F.data.startswith('join_'))
+async def join_selected(callback: CallbackQuery):
+    queue_id_str = callback.data.split('_')[1]
+    queue_id = int(queue_id_str)
+    queue_name = await rq.get_queue(queue_id)  # Retrieve the queue_name for the given queue_id
+    user_id = callback.message.from_user.id
+    chat_id = callback.message.chat.id
+    await callback.message.answer('You here')
+    if not await rq.join_queue(user_id, chat_id, queue_name):
+        await callback.message.answer(f'You follow the queue {queue_name}')
+    else:
+        await callback.message.answer(f'You already follow the queue {queue_name}')
+
